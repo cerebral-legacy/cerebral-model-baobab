85d5472bac81c7f6d4967bac816d10a5198b5d92
Jesse Wood <jess.nz@gmail.com>
2015-09-29 11:04:59 +1300

    add `return`statements to the tree mutations

--- index.js
+++ index.js
@@ -31,44 +31,44 @@ var Model = function (initialState, options) {
         },
         import: function (newState) {
           var newState = deepmerge(initialState, newState);
-          tree.set(newState);
+          return tree.set(newState);
         },
         mutators: {
           set: function (path, value) {
-            tree.set(path, value);
+            return tree.set(path, value);
           },
           unset: function (path) {
-            tree.unset(path);
+            return tree.unset(path);
           },
           push: function (path, value) {
-            tree.push(path, value);
+            return tree.push(path, value);
           },
           splice: function () {
             var args = [].slice.call(arguments);
-            tree.splice.call(tree, args.shift(), args);
+            return tree.splice.call(tree, args.shift(), args);
           },
           merge: function (path, value) {
             tree.merge(path, value);
           },
           concat: function () {
-            tree.apply(path, function (existingValue) {
+            return tree.apply(path, function (existingValue) {
               return existingValue.concat(value);
             });
           },
           pop: function (path) {
-            tree.apply(path, function (existingValue) {
+            return tree.apply(path, function (existingValue) {
               existingValue.pop();
               return existingValue;
             });
           },
           shift: function (path) {
-            tree.apply(path, function (existingValue) {
+            return tree.apply(path, function (existingValue) {
               existingValue.shift();
               return existingValue;
             });
           },
           unshift: function (path, value) {
-            tree.unshift(path, value);
+            return tree.unshift(path, value);
           }
         }
     };
